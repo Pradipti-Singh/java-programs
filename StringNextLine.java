2344f40602db552c16e4795815020b2a45875e50
PRADIPTI SINGH <91623073+Pradipti-Singh@users.noreply.github.com>
2023-05-29 13:33:52 +0530

    Create StringNextLine.java

---
+++ StringNextLine.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TakingInput {
+    public static void main(String[] args) {
+        System.out.println("Taking input from the user");
+        Scanner sc = new Scanner(System.in);
+        String b1 = sc.nextLine();
+        System.out.println(b1);
+    }
+}
+
+/*
+OUTPUT
+Taking input from the user
+Pradipti is a student.
+Pradipti is a student.
+*/
