27f5018ab0c00de742696a0696fd20c2e07f09d3
PRADIPTI SINGH <91623073+Pradipti-Singh@users.noreply.github.com>
2023-05-29 13:31:49 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TakingInput {
+    public static void main(String[] args) {
+        System.out.println("Taking input from the user");
+        Scanner sc = new Scanner(System.in);
+        String b1 = sc.next();
+        System.out.println(b1);
+    }
+}
+
+/*
+OUTPUT
+Taking input from the user
+Pradipti
+Pradipti
+*/
