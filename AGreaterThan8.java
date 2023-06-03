f6e368860e3e1368573a45771cfc2d59b12107a8
PRADIPTI SINGH <91623073+Pradipti-Singh@users.noreply.github.com>
2023-06-03 23:35:19 +0530

    Create AGreaterThan8.java

---
+++ AGreaterThan8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class AGreaterThan8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>8);
+    }
+}
+
+//OUTPUT
+/*
+9
+true
+*/
