929828ac638d40f286e1ee72a7bd4bec2379d83f
PRADIPTI SINGH <91623073+Pradipti-Singh@users.noreply.github.com>
2023-06-07 00:03:20 +0530

    Update ProblemSet_Strings.java

--- ProblemSet_Strings.java
+++ ProblemSet_Strings.java
@@ -11,5 +11,27 @@ public class ProblemSet_Strings {
         //Problem3- letter = "Dear <|name|>, Thanks a lot"
         String letter =  "Dear <|name|>, Thanks a lot";
         System.out.println(letter.replace("<|name|>","Radha"));
+
+        //Problem4 - detect double and triple spaces in a string
+        String myString = "Today is a     beautiful day";
+        String myString1 = "Today is a beautiful day";
+        System.out.println(myString.indexOf("  "));
+        System.out.println(myString.indexOf("   "));
+        System.out.println(myString1.indexOf("  "));
+        System.out.println(myString1.indexOf("   "));
+
+        //Problem5 - write the following format using escape sequence character in letter format
+        System.out.println("Dear Pradipti,\n\tThis Java course is nice.\n\tThanks");
     }
 }
+
+//OUTPUT
+/*
+10
+10
+-1
+-1
+Dear Pradipti,
+	This Java course is nice.
+	Thanks
+ */
