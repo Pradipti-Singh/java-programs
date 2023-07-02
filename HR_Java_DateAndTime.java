1cc48d4259ffdd2aba14762552b0d8e5209ae539
PRADIPTI SINGH <91623073+Pradipti-Singh@users.noreply.github.com>
2023-07-02 22:15:28 +0530

    Create HR_Java_DateAndTime.java

---
+++ HR_Java_DateAndTime.java
@@ -0,0 +1,73 @@
+/*
+The Calendar class is an abstract class that provides methods for converting between a specific instant in time and a set of calendar 
+fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the 
+next week.
+*/
+
+//Code
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.time.LocalDate;
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        LocalDate ld = LocalDate.of(year,month,day);
+       return ld.getDayOfWeek().name();
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+//output
+/*
+Input (stdin)
+08 05 2015
+
+Your Output (stdout)
+WEDNESDAY
+
+Expected Output
+WEDNESDAY
+*/
